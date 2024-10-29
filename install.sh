806125a46b0f13d2916708003190e4751cae5221
mosharoof98 <73770002+mosharoof98@users.noreply.github.com>
2024-10-29 22:30:50 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ opkg update
 opkg install iptables iptables-mod-nat-extra redsocks zram-swap
 
 #Then run this line
-service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://github.com/mosharoof98/BDIX-OpenWRT/raw/main/bdix.conf && cd /etc/init.d && wget https://github.com/mosharoof98/BDIX-OpenWRT/raw/main/bdix && chmod +x /etc/init.d/bdix
+service redsocks stop && cd /etc && wget https://github.com/mosharoof98/BDIX-OpenWRT/raw/main/bdix.conf && cd /etc/init.d && wget https://github.com/mosharoof98/BDIX-OpenWRT/raw/main/bdix && chmod +x /etc/init.d/bdix && rm /etc/redsocks.conf && rm /etc/init.d/redsocks
 
 cd /
 clear
